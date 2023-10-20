af508d68246b038ce67b4a23a330d4200dfbc675
faakit <andrealtosan@gmail.com>
2023-10-20 10:29:02 -0300

    feat: cross and line functions

--- ttt.asm
+++ ttt.asm
@@ -44,6 +44,7 @@ get_three_chars:
 	call cursor
     mov     ah, 7
     int     21h
+    mov     byte[buffer], al
     call    caracter
 	inc dl
     
@@ -51,6 +52,7 @@ get_three_chars:
     call cursor
 	mov     ah, 7
     int     21h
+    mov     byte[buffer+1], al
     call    caracter
     inc dl
 
@@ -58,6 +60,7 @@ get_three_chars:
 	call cursor
     mov     ah, 7
     int     21h
+    mov     byte[buffer+2], al
     call    caracter
     
     pop     dx
@@ -66,40 +69,201 @@ get_three_chars:
     pop     ax
     ret
 
-draw_cross:
-    push    ax
-    push    bx
-    push    cx
-    push    dx
-    
-    ; Draw the first line of the cross
-    mov     ax, 62
-    push    ax
-    mov     ax, 25
-    push    ax
-    mov     ax, 577
-    push    ax
-    mov     ax, 360
-    push    ax
-    call    line
-    
-    ; Draw the second line of the cross
-    mov     ax, 62
-    push    ax
-    mov     ax, 360
-    push    ax
-    mov     ax, 577
-    push    ax
-    mov     ax, 25
-    push    ax
-    call    line
-    
-    pop     dx
-    pop     cx
-    pop     bx
-    pop     ax
-    ret
+;_____________________________________________________________________________
+;    fun��o circle
+;	 push xc; push yc; call draw_circle;  (xc+30<639,yc+30<479)
+; cor definida na variavel cor
+draw_circle:
+	push 	bp
+	mov	 	bp,sp
+	pushf                        ;coloca os flags na pilha
+	push 	ax
+	push 	bx
+	push	cx
+	push	dx
+	push	si
+	push	di
+	
+	mov		ax,[bp+6]    ; resgata xc
+	mov		bx,[bp+4]    ; resgata yc
+	mov		cx,30
+	
+	mov 	dx,bx	
+	add		dx,cx       ;ponto extremo superior
+	push    ax			
+	push	dx
+	call plot_xy
+	
+	mov		dx,bx
+	sub		dx,cx       ;ponto extremo inferior
+	push    ax			
+	push	dx
+	call plot_xy
+	
+	mov 	dx,ax	
+	add		dx,cx       ;ponto extremo direita
+	push    dx			
+	push	bx
+	call plot_xy
+	
+	mov		dx,ax
+	sub		dx,cx       ;ponto extremo esquerda
+	push    dx			
+	push	bx
+	call plot_xy
+		
+	mov		di,cx
+	sub		di,1	 ;di=r-1
+	mov		dx,0  	;dx ser� a vari�vel x. cx � a variavel y
+	
+;aqui em cima a l�gica foi invertida, 1-r => r-1
+;e as compara��es passaram a ser jl => jg, assim garante 
+;valores positivos para d
 
+stay:				;loop
+	mov		si,di
+	cmp		si,0
+	jg		inf       ;caso d for menor que 0, seleciona pixel superior (n�o  salta)
+	mov		si,dx		;o jl � importante porque trata-se de conta com sinal
+	sal		si,1		;multiplica por doi (shift arithmetic left)
+	add		si,3
+	add		di,si     ;nesse ponto d=d+2*dx+3
+	inc		dx		;incrementa dx
+	jmp		plotar
+inf:	
+	mov		si,dx
+	sub		si,cx  		;faz x - y (dx-cx), e salva em di 
+	sal		si,1
+	add		si,5
+	add		di,si		;nesse ponto d=d+2*(dx-cx)+5
+	inc		dx		;incrementa x (dx)
+	dec		cx		;decrementa y (cx)
+	
+plotar:	
+	mov		si,dx
+	add		si,ax
+	push    si			;coloca a abcisa x+xc na pilha
+	mov		si,cx
+	add		si,bx
+	push    si			;coloca a ordenada y+yc na pilha
+	call plot_xy		;toma conta do segundo octante
+	mov		si,ax
+	add		si,dx
+	push    si			;coloca a abcisa xc+x na pilha
+	mov		si,bx
+	sub		si,cx
+	push    si			;coloca a ordenada yc-y na pilha
+	call plot_xy		;toma conta do s�timo octante
+	mov		si,ax
+	add		si,cx
+	push    si			;coloca a abcisa xc+y na pilha
+	mov		si,bx
+	add		si,dx
+	push    si			;coloca a ordenada yc+x na pilha
+	call plot_xy		;toma conta do segundo octante
+	mov		si,ax
+	add		si,cx
+	push    si			;coloca a abcisa xc+y na pilha
+	mov		si,bx
+	sub		si,dx
+	push    si			;coloca a ordenada yc-x na pilha
+	call plot_xy		;toma conta do oitavo octante
+	mov		si,ax
+	sub		si,dx
+	push    si			;coloca a abcisa xc-x na pilha
+	mov		si,bx
+	add		si,cx
+	push    si			;coloca a ordenada yc+y na pilha
+	call plot_xy		;toma conta do terceiro octante
+	mov		si,ax
+	sub		si,dx
+	push    si			;coloca a abcisa xc-x na pilha
+	mov		si,bx
+	sub		si,cx
+	push    si			;coloca a ordenada yc-y na pilha
+	call plot_xy		;toma conta do sexto octante
+	mov		si,ax
+	sub		si,cx
+	push    si			;coloca a abcisa xc-y na pilha
+	mov		si,bx
+	sub		si,dx
+	push    si			;coloca a ordenada yc-x na pilha
+	call plot_xy		;toma conta do quinto octante
+	mov		si,ax
+	sub		si,cx
+	push    si			;coloca a abcisa xc-y na pilha
+	mov		si,bx
+	add		si,dx
+	push    si			;coloca a ordenada yc-x na pilha
+	call plot_xy		;toma conta do quarto octante
+	
+	cmp		cx,dx
+	jb		fim_circle  ;se cx (y) est� abaixo de dx (x), termina     
+	jmp		stay		;se cx (y) est� acima de dx (x), continua no loop
+	
+	
+fim_circle:
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+	popf
+	pop		bp
+	ret		6
+
+    ; função para desenhar um X
+    ;  push x; push y; call draw_cross;
+draw_cross:
+	push 	bp
+	mov	 	bp,sp
+	pushf                        
+	push 	ax
+	push 	bx
+	push	cx
+	push	dx
+	
+	mov		cx,[bp+6]    ; resgata x
+	mov		dx,[bp+4]    ; resgata y
+    
+    ; desenha primeira linha da cruz
+    mov     ax, cx
+    add     ax, 30
+    push    ax
+    mov     ax, dx
+    add     ax, 30
+    push    ax
+    mov     ax, cx
+    sub     ax, 30
+    push    ax
+    mov     ax, dx
+    sub     ax, 30
+    push    ax
+    call    line
+    
+    ; desenha segunda linha da cruz
+    mov     ax, cx
+    add     ax, 30
+    push    ax
+    mov     ax, dx
+    sub     ax, 30
+    push    ax
+    mov     ax, cx
+    sub     ax, 30
+    push    ax
+    mov     ax, dx
+    add     ax, 30
+    push    ax
+    call    line
+
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+	popf
+	pop		bp
+	ret		1
 clear_screen:
     mov     ah, 0
     mov     al, 3
@@ -354,7 +518,10 @@ print33:
     inc     bx			;proximo caracter
     inc		dl			;avanca a coluna
     loop    print33
+
     ret     0
+; **********************************************************
+
 
 ;   fun��o plot_xy
 ;
@@ -640,6 +807,17 @@ campo_23    	db  		'23'
 campo_31    	db  		'31'
 campo_32    	db  		'32'
 campo_33    	db  		'33'
+buffer          db          '   '
+; 0 : nada. 1 : X. 2 : O
+campo_11_status db 1
+campo_12_status db 2
+campo_13_status db 0
+campo_21_status db 0
+campo_22_status db 0
+campo_23_status db 0
+campo_31_status db 0
+campo_32_status db 0
+campo_33_status db 0
 ;*************************************************************************
 segment stack stack
     		resb 		512
